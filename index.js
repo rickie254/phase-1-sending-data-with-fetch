0befd81441a1de4523a21763c44b49810e936065
rickie maweu <rickiemaweu@gmail.com>
2022-06-17 18:10:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const formData ={
+     name: "Steve",
+     email : "steve@steve.com",
+}
+function submitData() {
+
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch((error) => {
+        alert("Bad things!");
+      console.log(error.message);
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
